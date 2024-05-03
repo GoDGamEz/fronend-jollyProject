da6794adb17428f1f6176f29e1fd06578e48c1ef
GoDGamEz <chisanuchabest@hotmail.com>
2024-05-04 03:00:12 +0700

    commit

--- src/component/search.js
+++ src/component/search.js
@@ -52,14 +52,14 @@ const SearchBar = () => {
               type="text"
               value={searchTerm}
               onChange={(e) => handleSearch(e.target.value)}
-              className="ml-3 w-[175px] sm:w-[250px] md:w-[150px] lg:w-[225px] 2xl:w-[275px] h-[28px] outline-none placeholder-gray-800 text-[15px] lg:text-[16px] bg-[#fcfcfc]"
+              className="ml-3 w-[125px] sm:w-[250px] md:w-[150px] lg:w-[225px] 2xl:w-[275px] h-[28px] outline-none placeholder-gray-800 text-[15px] lg:text-[16px] bg-[#fcfcfc]"
               onClick={() => {
                 setShowSearch(true);
               }}
             />
             {showSearch && (<X className="w-5 h-5 lg:w-6 lg:h-6 items-end absolute right-5 top-1/2 transform -translate-y-1/2 cursor-pointer" onClick={() => setShowSearch(false)} />)}
           </div>
-          <div className={`block absolute px-9 w-[238px] sm:w-[318px] md:w-[218px] lg:w-[298px] 2xl:w-[348px] ml-0.5 mt-2 z-10 bg-white rounded-[18px] shadow-md overflow-y-auto overflow-hidden transition-max-h duration-300 ease-in-out ${showSearch ? 'max-h-[26.5vh]' : 'max-h-0'
+          <div className={`block absolute px-9 w-[188px] sm:w-[318px] md:w-[218px] lg:w-[298px] 2xl:w-[348px] ml-0.5 mt-2 z-10 bg-white rounded-[18px] shadow-md overflow-y-auto overflow-hidden transition-max-h duration-300 ease-in-out ${showSearch ? 'max-h-[26.5vh]' : 'max-h-0'
             }`}>
             <ul className="py-2">
               {data ? (
