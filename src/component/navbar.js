6a85806d86c19371e7123b21e339211d26c68cd5
GoDGamEz <chisanuchabest@hotmail.com>
2024-05-06 03:33:40 +0700

    commit

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -29,7 +29,7 @@ export default function Navbar() {
               <div className="relative flex h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                   {/* Mobile menu button*/}
-                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-800 hover:text-white outline-none">
                     <span className="absolute -inset-0.5" />
                     <span className="sr-only">Open main menu</span>
                     {open ? (
